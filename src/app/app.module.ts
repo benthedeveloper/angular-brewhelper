1af87933f691bb377bf5c79f17b5d6a37c666388
Ben Harrison <v-behar@microsoft.com>
2020-02-25 18:24:22 -0800

    Routes added for MyBrewsComponent, default route, and wildcard route
    Added style to active router link.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MyBrewsComponent } from './components/my-brews/my-brews.component';
 import { BrewListComponent } from './components/brew-list/brew-list.component';
 import { BrewDetailsComponent } from './components/brew-details/brew-details.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'my-brews', component: MyBrewsComponent },
+  { path: '',   redirectTo: '/my-brews', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MyBrewsComponent,
     BrewListComponent,
-    BrewDetailsComponent
+    BrewDetailsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
